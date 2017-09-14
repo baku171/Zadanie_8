8d4ba8a55008f4f3b07e4bdd1cc6e0fc7adf999c
baku171 <pbak@esri.pl>
2017-09-14 14:23:07 +0200

    Instrukcje warunkowe

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // scripts.js
 
-var name = prompt('Enter your name');
+//var name = prompt('Enter your name');
 //alert('Hello, ' + name);
 console.log('Hello, ' + name);
 
@@ -9,9 +9,21 @@ console.log('Hello, ' + name);
 /*
 Komentarz blokowy
 */
+//Zmienne
 var a = prompt('Podaj długość podstawy'),
-    h = prompt('Podaj wysokość')
-    triangleArea = a*h/2;
-
+    h = prompt('Podaj wysokość'),
+    b = prompt('Podaj wartość b'),
+    triangleArea = a*h/2,
+    value = ((a * a) + (2 * a * b) - (b * b));
+//Pole trójkąta
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-alert('Pole trójkąta wynosi: ' + triangleArea)
+alert('Pole trójkąta wynosi: ' + triangleArea);
+//Warunki
+console.log('Wynik działania wynosi: ' + value);
+if (value > 0) {
+    console.log('Wynik dodatni')
+} else if (value < 0) {
+    console.log('Wynik ujemny')
+} else {
+    console.log('Wynik zerowy')
+}
