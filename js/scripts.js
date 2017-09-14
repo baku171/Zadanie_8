d0d01fdd82d82110b85f9cf80a34f71359e5f529
baku171 <pbak@esri.pl>
2017-09-14 13:19:35 +0200

    Triangle area

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,9 @@ console.log('Hello, ' + name);
 /*
 Komentarz blokowy
 */
+var a = prompt('Podaj długość podstawy'),
+    h = prompt('Podaj wysokość')
+    triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Pole trójkąta wynosi: ' + triangleArea)
