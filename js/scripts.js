f99ee0eca5bc2c5ad880a4996376c2a0907b46b0
baku171 <pbak@esri.pl>
2017-09-15 07:19:09 +0200

    Poprawione zadania

--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,17 @@ Komentarz blokowy
 var a = prompt('Podaj długość podstawy'),
     h = prompt('Podaj wysokość'),
     b = prompt('Podaj wartość b'),
-    triangleArea = a*h/2,
-    value = ((a * a) + (2 * a * b) - (b * b));
+    triangleArea = a * h / 2,
+    value = (a * a) + (2 * a * b) - (b * b);
 //Pole trójkąta
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 alert('Pole trójkąta wynosi: ' + triangleArea);
 //Warunki
 console.log('Wynik działania wynosi: ' + value);
 if (value > 0) {
-    console.log('Wynik dodatni')
+    console.log('Wynik dodatni');
 } else if (value < 0) {
-    console.log('Wynik ujemny')
+    console.log('Wynik ujemny');
 } else {
-    console.log('Wynik zerowy')
+    console.log('Wynik zerowy');
 }
