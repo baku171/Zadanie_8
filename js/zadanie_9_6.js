a8a777c881b244cb4e57127f5b2da7f8f3cd3b8d
baku171 <pbak@esri.pl>
2017-09-21 15:13:23 +0200

    Poprawione 9_6

--- js/zadanie_9_6.js
+++ js/zadanie_9_6.js
@@ -3,7 +3,8 @@ var list = document.getElementById('list');
 
 button.addEventListener("click", function() {
     var element = document.createElement("li");
-    var elementNumber = document.getElementsByTagName("li").length;
-    element.innerHTML = 'item ' + (elementNumber - 4);
+    var elementNumber = list.getElementsByTagName("li").length;
+    // Odejmuję 4 żeby nie usuwać li z html'a.
+    element.innerHTML = 'item ' + elementNumber;
     list.appendChild(element);
 });
