445383f4b8f4f349bca999e59c7fefb064f3f608
baku171 <pbak@esri.pl>
2017-09-21 09:48:39 +0200

    Zadanie 9_4

--- js/zadanie_9_4.js
+++ js/zadanie_9_4.js
@@ -1,6 +1,13 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur = 'triceratops';
-var dinosaurUppercase = dinosaur.toUpperCase();
-var changedText = text.replace('Velociraptor', dinosaurUppercase);
+var levels = 5;
+function drawTree(levels) {
+    for (var i = 0; i<=levels; i++) {
+        var star = '';
+        for (var j = 0; j<=i; j++) {
+            star += '*';
+        }
+        console.log(star)
+    }
+}
 
-console.log(changedText.substr(0, changedText.length / 2));
+console.log(drawTree(4));
+console.log(drawTree(9));
